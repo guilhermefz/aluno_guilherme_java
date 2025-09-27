566541985fe3a321d507fe4c960ebaaaf312170c
Guilherme Fiuza <guilherme_f_campos@estudante.sesisenai.org.br>
2025-09-26 22:49:14 -0300

    refatoração

--- src/main/java/com/escola/ALUNO_guilherme/dtos/AlunoDto.java
+++ src/main/java/com/escola/ALUNO_guilherme/dtos/AlunoDto.java
@@ -2,13 +2,16 @@ package com.escola.ALUNO_guilherme.dtos;
 
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class AlunoDto {
-    @NotBlank(message = "O campo nome é obrigatório")
+    @NotBlank(message = "O nome é obrigatório")
+    @Size(min = 3, message = "O nome deve ter no mínimo 3 caracteres")
     private String nome;
     private String telefone;
-    @NotBlank(message = "O campo curso é obrigatório")
+    @NotBlank(message = "O curso é obrigatório")
+    @Size(min = 3, message = "O curso deve ter no mínimo 3 caracteres")
     private String curso;
 }
